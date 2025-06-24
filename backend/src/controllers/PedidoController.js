47ad3b0f67e134d532f7d5d4dd12bcda4cd3da9c
rafael.leal <rafaellealg18@gmail.com>
2025-06-24 19:17:05 -0300

    add controller

---
+++ backend/src/controllers/PedidoController.js
@@ -0,0 +1,34 @@
+import repositorioGeral from "../model/repositories/repositorioGeral.js";
+import Pedido from '../model/entities/Pedido.js';
+const repo = new repositorioGeral();
+
+const CadastroPedidoController = {
+  cadastrarPedido: async (req, res) => {
+    const { id_usuario_cliente, id_usuario_servidor, id_servico, data } = req.body;
+  
+    var pedido = new Pedido();
+    
+    pedido.id_usuario_cliente=id_usuario_cliente;
+    pedido.id_usuario_servidor=id_usuario_servidor;
+    pedido.id_servico=id_servico;
+    pedido.data=data;
+
+    // Simulação de lógica — aqui você chamaria o repository, validaria etc.
+    if (!id_usuario_cliente ||!id_usuario_servidor || !id_servico || !data) {
+      return res
+        .status(400)
+        .json({ erro: "Todos os campos são obrigatórios." });
+    }
+
+    // Lógica de criação fictícia
+    
+    await repo.addPedido(pedido);
+    console.log("Pedido cadastrados");
+    console.log("Pedido cadastrado:", { id_usuario_cliente, id_usuario_servidor, id_servico, data });
+    return res
+      .status(201)
+      .json({ mensagem: "Pedido cadastrado com sucesso.",servico:nome });
+  },
+};
+
+export default CadastroPedidoController;
