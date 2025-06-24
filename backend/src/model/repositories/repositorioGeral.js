ca59819b8ae265cbc2277b78d2ccaa9d3b2af463
rafael.leal <rafaellealg18@gmail.com>
2025-06-24 19:19:46 -0300

    add pedido in general repository

--- backend/src/model/repositories/repositorioGeral.js
+++ backend/src/model/repositories/repositorioGeral.js
@@ -1,15 +1,20 @@
 import UsuarioRepository from "./UsuarioRepository.js";
 import ServicoRepository from "./ServicoRepository.js";
+import PedidoRepository from "./PedidoRepository.js";
 class repositorioGeral {
     constructor(){
      this.UserRepository= new UsuarioRepository();
      this.ServiceRepository= new ServicoRepository();   
+     this.PedidoRepository= new PedidoRepository();   
     } 
     addUser(usuario){
         this.UserRepository.setUsuario(usuario)
     }
     async addServico(servico){
         await this.ServiceRepository.setServico(servico)
+    }
+    async addPedido(pedido){
+        await this.PedidoRepository.setPedido(pedido)
     }
 }
 export default repositorioGeral;
