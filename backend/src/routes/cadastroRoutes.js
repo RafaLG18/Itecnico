4e6690d6bacc5eb876e186e6ae2c45838cc9c74f
rafael.leal <rafaellealg18@gmail.com>
2025-07-01 08:50:16 -0300

    remove unnecessary files

--- backend/src/routes/cadastroRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import CadastroUsuarioController from "../controllers/CadastroUsuarioController.js";
-
-const router = express.Router();
-
-// POST /api/cadastro
-router.post("/", CadastroUsuarioController.cadastrarUsuario);
-
-export default router;
